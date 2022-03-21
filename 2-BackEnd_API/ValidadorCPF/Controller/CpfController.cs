f764d1a88f01bbebef8e641d61a2837f1d8e898d
Thiago Teodoro Repizo <91916228+RepizoThiago1@users.noreply.github.com>
2022-03-20 23:50:22 -0300

    Add files via upload

---
+++ 2-BackEnd_API/ValidadorCPF/Controller/CpfController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ValidadorCPF.Service;
+
+
+namespace ValidadorCPF.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CpfController : ControllerBase
+    {
+        public IValidateCpfService _validateCpfService;
+        public CpfController(IValidateCpfService validateCpfService)
+        {
+            _validateCpfService = validateCpfService;
+        }
+        [HttpPost("ValidarCPF")]
+        public bool ValidarCPF(string cpf)
+        {
+            _validateCpfService.IsCpf(cpf);
+
+            if (_validateCpfService.IsCpf(cpf))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
